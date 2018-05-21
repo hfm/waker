dd40bdbf6b14e4a7cf611d72defcfbca321c84fa
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-05-22 07:13:51 +0900

    Add maintenance scope specs

--- spec/models/maintenance_spec.rb
+++ spec/models/maintenance_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Maintenance, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Maintenance, type: :model do
+  let(:topic) { create(:topic) }
+
+  describe 'scope' do
+    let(:maintenance) { create(:maintenance, start_time: Time.now, end_time: Time.now + 1) }
+
+    describe 'active' do
+      subject { Maintenance.active }
+      it { is_expected.to include maintenance }
+    end
+
+    describe 'not_expired' do
+      subject { Maintenance.not_expired }
+      it { is_expected.to include maintenance }
+    end
+
+    describe 'expired' do
+      subject { Maintenance.expired }
+      it { is_expected.not_to include maintenance }
+    end
+  end
 end
